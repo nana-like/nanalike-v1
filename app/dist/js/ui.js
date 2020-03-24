772ba2a60ad6f054104135af37745d3c4a69b757
nana <nykim@nykim.net>
2020-03-24 12:24:31 +0900

    add popup

--- app/dist/js/ui.js
+++ app/dist/js/ui.js
@@ -1,50 +1,75 @@
-// console.log("팝업연결하기!");
-
-// var btnPopup = document.querySelector(".js-open-popup");
-
-// var clickHandler = function() {
-//   modalEvt.show();
-// };
-
-// var modal = document.querySelector(".modal");
-
-// var modalEvt = {
-//   show: function() {
-//     console.log("모달 보여짐");
-//     modal.classList.add("modal--opened");
-//   },
-//   hide: function() {
-//     console.log("모달 닫힘");
-//   }
-// };
-
-// btnPopup.addEventListener("click", clickHandler);
-
-var modal = document.querySelector(".modal");
-
-var btnModalClose = modal.querySelector(".btn-modal-close"),
-  btnModalOkay = modal.querySelector(".btn-modal-okay"),
+var wrap = document.querySelector(".wrap");
+var header = document.querySelector(".header__inner");
+var modal = document.querySelector(".modal"),
+  btnModalClose = modal.querySelector(".btn-modal-close"),
   modalDim = modal.querySelector(".modal__dim");
+
+// 💪 해당 브라우저의 스크롤바 너비를 구하는 함수
+var getScrollBarWidth = function() {
+  document.body.style.overflow = "hidden";
+  var width = document.body.clientWidth;
+  document.body.style.overflow = "scroll";
+  width -= document.body.clientWidth;
+  if (!width) width = document.body.offsetWidth - document.body.clientWidth;
+  document.body.style.overflow = "";
+  return width;
+};
+
+// 💪 스크롤을 막는 함수
+var preventScroll = function(type) {
+  var body = document.body;
+  var paddingR = getScrollBarWidth();
+
+  // 스크롤바가 생길 만큼 길지 않은 경우, 이 이벤트를 무시
+  if (body.scrollHeight <= body.clientHeight) {
+    return false;
+  }
+
+  // 스크롤을 막는 클래스네임을 body에 추가
+  document.body.classList.add("scroll-disabled");
+
+  // 💪 사라진 스크롤바 만큼의 패딩값을 추가해 주는 함수
+  // .scroll-disabled가 추가되면 윈도우의 브라우저에서는 스크롤바가 사라지기 때문에 페이지 밀림 현상이 발생하는데, 이를 막아주기 위함 (모바일, 맥에서는 스크롤바가 보통 숨김 상태라 무관)
+
+  function getPadding() {
+    for (var i = 0; i < arguments.length; i++) {
+      arguments[i].style.paddingRight = paddingR + "px";
+    }
+  }
+
+  getPadding(wrap);
+  getPadding(header);
+  wrap.style.backgroundColor = "#ffffff";
+  // header.style.paddingRight = 75 + "px";
+};
+
+// 💪 스크롤을 허용하는 함수
+var allowScroll = function() {
+  wrap.style.paddingRight = 0;
+  header.style.paddingRight = 0 + "px";
+  wrap.style.backgroundColor = "";
+  document.body.classList.remove("scroll-disabled");
+};
+
+// 💪 모달 여닫는 함수
 var modalEvt = {
   open: function(e) {
     e.preventDefault();
+    preventScroll();
     modal.classList.add("modal--opened");
-    // 👆 .modal에 .-modal-opened가 붙으면 화면에 보입니다.
   },
   close: function() {
+    allowScroll();
     modal.classList.remove("modal--opened");
   }
 };
-if (btnModalOkay != null) {
-  btnModalOkay.addEventListener("click", modalEvt.close);
-  // 👆 확인 버튼 클릭 시 모달을 닫습니다.
-}
+
 btnModalClose.addEventListener("click", modalEvt.close);
 // 👆 닫기 버튼 클릭 시 모달을 닫습니다.
 modalDim.addEventListener("click", modalEvt.close);
 // 👆 딤(검정 배경) 클릭 시 모달을 닫습니다.
 
-// !! 🤓 TEST 🤓 !!
+// !! 🤓
 var btnPopup = document.querySelector(".js-open-popup");
 btnPopup.addEventListener("click", modalEvt.open);
 
